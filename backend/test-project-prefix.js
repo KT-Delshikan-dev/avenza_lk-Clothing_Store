3c108fd4b34234f9104dd3fd8290c41322ca8439
KT-Delshikan-dev <delshikanktofficial@gmail.com>
2026-05-05 14:24:56 +0530

    chore: remove hardcoded appwrite test credentials

--- backend/test-project-prefix.js
+++ backend/test-project-prefix.js
@@ -1,8 +1,9 @@
 const { Client, Databases } = require('node-appwrite');
+require('dotenv').config();
 
-const projectId = 'sgp-69f1cb840006d2bde03e';
-const apiKey = '-standard_750399933bcb2e139e91e237ff8ba3562019fe8e9acc3a20fb950febf8f2f54b5cc818e165b06accb99a04ff7a22728be615a15a6908a9706910f6ff5852e1974acf4c804e89ec806f90ac20917fc95b86ba413fc8858af92d1490da9e8c3f53a16fefb501c9b6d233da0616f55a525389cdcea4d5cf42c456c9b88f74cde6e1';
-const endpoint = 'https://sgp.cloud.appwrite.io/v1';
+const projectId = process.env.APPWRITE_PROJECT_ID;
+const apiKey = process.env.APPWRITE_API_KEY;
+const endpoint = process.env.APPWRITE_ENDPOINT;
 
 async function testProjectIdPrefix() {
     const client = new Client()
